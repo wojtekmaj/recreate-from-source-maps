5fa401a9c36e8fa3ca484f7e2607ce03784209f5
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-01-23 17:45:52 +0100

    Find bundles in Webpack bootstrap even if jsonpScriptSrc is not found

--- src/get_bundles_from_bootstrap.js
+++ src/get_bundles_from_bootstrap.js
@@ -1,30 +1,72 @@
-const { makeProgress } = require('./log');
+const { log, makeProgress } = require('./log');
 
-const getBundlesFromBootstrap = async (bootstrap, scriptUrl) => {
+const getBundlesFromBootstrap = async (bootstrap, sampleScriptUrl) => {
   const findingBundles = makeProgress('Finding bundles');
-  const beginOfFunction = '\t// script path function';
-  const jsonpScriptSrcBody = bootstrap.slice(bootstrap.indexOf(beginOfFunction)).split('\n').slice(1, 4).join('\n');
-  const configLine = jsonpScriptSrcBody
-    .split('\n')
-    .slice(1, -1)[0];
-  const config = JSON.parse(configLine.slice(configLine.indexOf('{'), configLine.indexOf('}') + 1));
 
-  const beginOfPublicPath = '\t// __webpack_public_path__';
-  const webpackPublicPath = bootstrap.slice(bootstrap.indexOf(beginOfPublicPath)).split('\n').slice(1, 2).join('\n');
+  const jsonpScriptSrc = bootstrap.includes('jsonpScriptSrc');
+  const scriptSrc = bootstrap.includes('script.src = __webpack_require__.p');
 
-  const __webpack_require__ = {}; // eslint-disable-line
-  eval(webpackPublicPath); // eslint-disable-line no-eval
-  eval(jsonpScriptSrcBody); // eslint-disable-line no-eval
-  if (!__webpack_require__.p) {
-    throw new Error('Failed to determine publicPath');
-  }
-  if (!jsonpScriptSrc) { // eslint-disable-line no-undef
-    throw new Error('Failed to create jsonpScriptSrc function');
-  }
+  const chunkIds = (() => {
+    if (jsonpScriptSrc) {
+      const beginOfFunction = '\t// script path function';
+      const jsonpScriptSrcBody = bootstrap.slice(bootstrap.indexOf(beginOfFunction)).split('\n').slice(1, 4).join('\n');
+      const configLine = jsonpScriptSrcBody
+        .split('\n')
+        .slice(1, -1)[0];
+      const config = JSON.parse(configLine.slice(configLine.indexOf('{'), configLine.indexOf('}') + 1));
+      return Object.keys(config);
+    }
 
-  const chunkIds = Object.keys(config);
-  const bundleFilenames = chunkIds.map(jsonpScriptSrc); // eslint-disable-line no-undef
-  const bundleUrls = bundleFilenames.map(bundleUrl => new URL(bundleUrl, scriptUrl).toString());
+    if (scriptSrc) {
+      // TODO: How to detect that?
+      return [];
+    }
+
+    return [];
+  })();
+
+  const chunkIdToBundleFilename = (() => {
+    const beginOfPublicPath = '\t// __webpack_public_path__';
+    const webpackPublicPath = bootstrap.slice(bootstrap.indexOf(beginOfPublicPath)).split('\n').slice(1, 2).join('\n');
+
+    const __webpack_require__ = {}; // eslint-disable-line
+    eval(webpackPublicPath); // eslint-disable-line no-eval
+    if (!__webpack_require__.p) {
+      throw new Error('Failed to determine publicPath');
+    }
+
+    if (jsonpScriptSrc) {
+      const beginOfFunction = '\t// script path function';
+      const jsonpScriptSrcBody = bootstrap.slice(bootstrap.indexOf(beginOfFunction)).split('\n').slice(1, 4).join('\n');
+
+      eval(jsonpScriptSrcBody); // eslint-disable-line no-eval
+      if (!jsonpScriptSrc) {
+        throw new Error('Failed to create jsonpScriptSrc function');
+      }
+
+      return jsonpScriptSrc;
+    }
+
+    if (scriptSrc) {
+      const beginOfLine = 'script.src = __webpack_require__.p';
+      const textToCut = 'script.src = ';
+      const scriptSrcLine = bootstrap.slice(bootstrap.indexOf(beginOfLine) + textToCut.length).split('\n').slice(0, 1)[0];
+
+      eval(`function scriptSrc(chunkId) { return ${scriptSrcLine} }`); // eslint-disable-line no-eval
+      if (!scriptSrc) {
+        throw new Error('Failed to create scriptSrc function');
+      }
+
+      return scriptSrc;
+    }
+
+    return null;
+  })();
+
+  const bundleFilenames = chunkIds.map(chunkIdToBundleFilename); // eslint-disable-line no-undef
+  const bundleUrls = bundleFilenames.map(
+    bundleUrl => new URL(bundleUrl, sampleScriptUrl).toString(),
+  );
   findingBundles.done(`Found ${bundleUrls.length} bundles.`);
 
   return bundleUrls;
