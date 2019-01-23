51d34efa08f4b48e718931d3590e8ac62a0084dc
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-01-23 17:46:07 +0100

    Ensure files won't escape results folder

--- src/write_files.js
+++ src/write_files.js
@@ -4,7 +4,11 @@ const mkdirp = require('mkdirp');
 
 module.exports = (files, projectName) => new Promise((resolve) => {
   Object.entries(files).forEach(([fileName, data]) => {
-    const filePath = path.join('results', projectName, fileName);
+    let safeFileName = fileName;
+    while (safeFileName.startsWith('../')) {
+      safeFileName = safeFileName.slice(3);
+    }
+    const filePath = path.join('results', projectName, safeFileName);
     const fileDir = path.join(...filePath.split('/').slice(0, -1));
     if (!fs.existsSync(fileDir)) {
       mkdirp.sync(fileDir);
